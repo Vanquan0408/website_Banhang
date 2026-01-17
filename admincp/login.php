fbd1c81b3c70994977773c80c33e73c6e1897324
Vanquan0408 <vanquan094833@gmail.com>
2026-01-17 10:38:07 +0700

    code củ

--- admincp/login.php
+++ admincp/login.php
@@ -139,6 +139,6 @@ p {
     </table>
     </form>
 </div>  
-<script> type"text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"</script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
 </html>
